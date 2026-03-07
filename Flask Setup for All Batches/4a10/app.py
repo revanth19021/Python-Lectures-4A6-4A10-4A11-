17c8231f385aaaf7bc576f10a8e6a8187f1927ab
revanth19021 <revanth200319@gmail.com>
2026-03-07 14:26:26 +0530

    Committed

--- Flask Setup for All Batches/4a10/app.py
+++ Flask Setup for All Batches/4a10/app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 from pymongo import MongoClient
 
 client=MongoClient('mongodb+srv://revanth200319:revanth200319@cluster0.zrtypbn.mongodb.net/')
@@ -11,18 +11,61 @@ app=Flask(__name__)
 def home():
     return render_template('first.html')
 
-@app.route('/revanth')
-def home1():
-    return render_template('second.html')
+
 
 @app.route('/register')
 def registration():
     return render_template('register.html')
 
 
+@app.route('/registration',methods=['post'])
+def registeruser():
+    #a= request.form.get('email')
+    a=request.form['email']
+    b=request.form['password']
+    c=request.form['cnfpass']
+    d=request.form['username']
+    # cchecking the password 
+    if b!=c:
+        return "Please enter correct password"
+    # Existed user 
+    user=collection.find_one({
+        'username':d
+    })
+    
+    if user:
+        return "Already Exisisted User"
+    
+    collection.insert_one({
+        'email_id':a,
+        'password':b,
+        'username':d
+    })
+    return "Registered Successfully"
+
 @app.route('/login')
-def loginpage():
+def log():
     return render_template('login.html')
+
+@app.route('/login-verify',methods=['post'])
+def loginverification():
+    a=request.form['username']
+    # b=request.form['password']
+    user=collection.find_one({
+        'username':a
+    })
+    
+    if user:
+        return render_template('second.html')
+    
+    return render_template('login.html')
+    
+    
+    
+    
+    
+    
+
 
 if __name__=='__main__':
     app.run(port=5500,debug=True)
